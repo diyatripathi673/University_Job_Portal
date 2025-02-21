feaae3559db9b3c863619811aab2593404b537c7
Diya Tripathi <tripathidiya673@gmail.com>
2025-02-22 01:11:52 +0530

    CategoryCarousel complete

---
+++ Frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+
+const category = [
+  "Frontend Developer",
+  "Backend Devloper",
+  "Data Scince",
+  "Graphic Designer",
+  "FullStack Devloper",
+];
+const CategoryCarousel = () => {
+  return (
+    <div>
+
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => {
+            return(
+                <CarouselItem className="md: basis-1/2 lgbasis-1/3">
+                <Button  variant="outline" className="rounded-full bg-slate-200">{cat}</Button>
+            </CarouselItem>
+            )
+          })}
+        </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext/>
+       
+      </Carousel>
+    </div>
+  );
+};
+
+export default CategoryCarousel;
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
 import Navbar from "./shared/Navbar"
 import Herosection from "./Herosection"
+import CategoryCarousel from "./CategoryCarousel"
 
 export default function Home() {
   return (
     <div>
     <Navbar/>
     <Herosection/>
+    <CategoryCarousel/>
     </div>
   )
 }
